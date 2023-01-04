2dc700bf09657110ad112d3148bf9d786bf1daf3
Marat Khagazheev <marathagazeev@gmail.com>
2023-01-04 19:14:36 +0300

    added: first enemy, main menu, pause, fps, stats bar

--- game.py
+++ game.py
@@ -4,8 +4,8 @@ import random
 from settings import *
 from characters import *
 from textures import Tile
+from typing import Tuple
 from camera import Camera
-
 
 camera = Camera()
 
@@ -15,33 +15,97 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.tiles = pygame.sprite.Group()
         self.hero_sprite = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.electro_enemies = pygame.sprite.Group()
         self.alchemists = pygame.sprite.Group()
+        self.tiles_arr = []
         for y in range(13):
+            temp = []
             for x in range(20):
                 num = random.randint(0, 1)
                 if num:
-                    Tile("large hell stone", 100 * x, 100 * y, self.tiles, self.all_sprites)
+                    tile = Tile("large hell stone", 100 * x, 100 * y, self.tiles, self.all_sprites)
                 else:
-                    Tile("small hell stone", 100 * x, 100 * y, self.tiles, self.all_sprites)
+                    tile = Tile("small hell stone", 100 * x, 100 * y, self.tiles, self.all_sprites)
+                temp.append(tile)
+            self.tiles_arr.append(temp)
+        ElectroEnemy((500, 250), self.enemies, self.electro_enemies, self.all_sprites)
         Alchemist((250, 250), self.alchemists, self.all_sprites)
         self.hero = Hero((500, 500), self.hero_sprite, self.all_sprites)
+        self.is_paused = False
 
-    def draw_sprites(self, screen: pygame.Surface):
+    def draw_sprites(self, screen: pygame.Surface) -> None:
         """Drawing all tiles"""
         self.all_sprites.draw(screen)
 
-    def update_alchemists_images(self):
+    def update_alchemists_images(self) -> None:
         """Updating the alchemists picture"""
-        for sprite in self.alchemists:
-            sprite.update_image()
+        if not self.is_paused:
+            for sprite in self.alchemists:
+                sprite.update_image()
 
-    def update_heros_image(self):
+    def update_heros_image(self) -> None:
         """Updating the heros image"""
-        for sprite in self.hero_sprite:
-            sprite.update_image()
+        if not self.is_paused:
+            for sprite in self.hero_sprite:
+                sprite.update_image()
 
-    def update(self):
-        self.hero.update_position()
-        camera.update(self.hero)
-        for sprite in self.all_sprites:
-            camera.apply(sprite)
+    def update_electro_enemies_image(self) -> None:
+        if not self.is_paused:
+            for sprite in self.electro_enemies:
+                sprite.update_image()
+
+    def is_free(self, position: Tuple[int, int]) -> bool:
+        return self.tiles_arr[position[1] // 100][position[0] // 100].type in FREE_TYLES
+
+    def is_hero_in_sight(self, enemy_position: Tuple[int, int], hero_position: Tuple[int, int]) -> bool:
+        return abs(hero_position[0] - enemy_position[0]) <= 400 and abs(hero_position[1] - enemy_position[1]) <= 400
+
+    def find_path_step(self, start: Tuple[int, int], target: Tuple[int, int]) -> Tuple[int, int]:
+        step_size = 15
+        if start[0] < target[0] and start[1] < target[1] and self.is_free((start[0] + step_size, start[1] + step_size)):
+            return start[0] + step_size, start[1] + step_size
+        elif start[0] == target[0] and start[1] < target[1] and self.is_free((start[0], start[1] + step_size)):
+            return start[0], start[1] + step_size
+        elif start[0] == target[0] and start[1] > target[1] and self.is_free((start[0], start[1] - step_size)):
+            return start[0], start[1] - step_size
+        elif start[0] < target[0] and start[1] == target[1] and self.is_free((start[0] + step_size, start[1])):
+            return start[0] + step_size, start[1]
+        elif start[0] > target[0] and start[1] == target[1] and self.is_free((start[0] - step_size, start[1])):
+            return start[0] - step_size, start[1]
+        elif start[0] > target[0] and start[1] > target[1] and self.is_free(
+                (start[0] - step_size, start[1] - step_size)):
+            return start[0] - step_size, start[1] - step_size
+        elif start[0] < target[0] and start[1] < target[1] and self.is_free(
+                (start[0] + step_size, start[1] + step_size)):
+            return start[0] + step_size, start[1] + step_size
+        elif start[0] < target[0] and start[1] > target[1] and self.is_free(
+                (start[0] + step_size, start[1] - step_size)):
+            return start[0] + step_size, start[1] - step_size
+        elif start[0] > target[0] and start[1] < target[1] and self.is_free(
+                (start[0] - step_size, start[1] + step_size)):
+            return start[0] - step_size, start[1] + step_size
+        return start
+
+    def move_enemies(self) -> None:
+        if not self.is_paused:
+            for enemy in self.enemies:
+                if self.is_hero_in_sight(enemy.get_position(), self.hero.get_position()):
+                    next_position = self.find_path_step(enemy.get_position(), self.hero.get_position())
+                    enemy.set_position(next_position)
+                    enemy.is_running = True
+                else:
+                    enemy.is_running = False
+
+    def can_enemy_attack(self, obj) -> bool:
+        pass
+
+    def update_enemies(self) -> None:
+        pass
+
+    def update(self) -> None:
+        if not self.is_paused:
+            self.hero.update_position()
+            camera.update(self.hero)
+            for sprite in self.all_sprites:
+                camera.apply(sprite)
--- settings.py
+++ settings.py
@@ -2,19 +2,26 @@ import pygame
 
 from image_loader import load_image
 
-
 pygame.mixer.init()
 
 FPS = 60
 
 TILE_SIZE = (100, 100)
 ALCHEMIST_SIZE = (100, 100)
+ELECTRO_ENEMY_SIZE = (150, 150)
+
+HERO_MOVE_SPEED = 10
 
 HERO_IMAGE_UPDATE_EVENT_TYPE = 30
 HERO_STEP_SOUND_EVENT_TYPE = 31
 ALCHEMIST_EVENT_TYPE = 40
+ELECTRO_ENEMY_EVENT_TYPE = 50
+ELECTRO_ENEMY_MOVE_EVENT_TYPE = 51
 
 MENU_IMAGE = pygame.transform.scale(load_image("menu_image.png"), (1920, 1080))
+CURSOR_IMAGE = pygame.transform.scale(load_image("cursor.png"), (50, 50))
+
+FREE_TYLES = ["small hell stone", "large hell stone", "grass"]
 
 TILE_IMAGES = {
     "grass": load_image("grass.png"),
@@ -43,14 +50,29 @@ HERO_IMAGES = {
                         pygame.transform.scale(load_image("attacking_hero5.png"), (150, 150))],
 }
 
+ELECTRO_ENEMY_IMAGES = {
+    "right_standing": [pygame.transform.scale(load_image("elecenemy1.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy2.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy3.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy4.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy5.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy6.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy7.png"), (150, 150)),
+                       pygame.transform.scale(load_image("elecenemy8.png"), (150, 150))],
+    "right_running": [pygame.transform.scale(load_image("running_elecenemy1.png"), (150, 150)),
+                      pygame.transform.scale(load_image("running_elecenemy2.png"), (150, 150)),
+                      pygame.transform.scale(load_image("running_elecenemy3.png"), (150, 150)),
+                      pygame.transform.scale(load_image("running_elecenemy4.png"), (150, 150)),
+                      pygame.transform.scale(load_image("running_elecenemy5.png"), (150, 150)),
+                      pygame.transform.scale(load_image("running_elecenemy6.png"), (150, 150))]
+}
+
 ALCHEMIST_IMAGES = {
     "right": [pygame.transform.scale(load_image("alchemist.png"), ALCHEMIST_SIZE),
               pygame.transform.scale(load_image("alchemist2.png"), ALCHEMIST_SIZE)]
 }
 
-
 MENU_SOUNDS = {"hover": pygame.mixer.Sound('sounds/menu/hover.wav')}
 HERO_SOUNDS = {"step": pygame.mixer.Sound('sounds/hero/step.wav'),
                "attack1": pygame.mixer.Sound('sounds/hero/attack1.wav'),
                "attack2": pygame.mixer.Sound('sounds/hero/attack2.wav')}
-
